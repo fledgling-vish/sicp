f1cdbe589b5b2b586021de39d03e5d53102968ee
Vishal Thukral <vthukral94@gmail.com>
2021-04-26 10:55:50 -0700

    main.scm deleted

--- 6-001/projects/p1/main.scm
+++
@@ -1,9 +0,0 @@
-;;; https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-001-structure-and-interpretation-of-computer-programs-spring-2005/projects/project1.pdf
-
-;;; Problem 1
-(define position; computes position of ball at time t, where a is initial acceleration, u is initial position, v is initial velocity & t is time
-  (lambda (a v u t)
-    (+
-     u
-     (* v t)
-     (* (/ 1 2) a (square t)))))
